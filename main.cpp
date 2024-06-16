90368a8e60ee703aef6b23c7e8cb8b19feda46b9
b4bf00ln4mu <boy566@naver.com>
2024-06-17 00:10:22 +0900

    new와 포인터 벡터 완벽이해

--- main.cpp
+++ main.cpp
@@ -1,8 +1,83 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<conio.h>
+#include "Player.h"
+#include "Monster.h"
+#include "Goal.h"
+#include "Wall.h"
+
+using namespace std;
 
 int main()
 {
+	_getch();
+	int X = 0;
+	int Y = 0;
 
 
-	return 0;
+
+	int Map[10][10] =
+	{
+	{ 1,1,1,1,1,1,1,1,1,1 },
+	{ 1,2,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,1 },
+	{ 1,1,1,1,1,1,1,1,1,1 }
+	};
+	vector<Actor*> MyActor;
+
+	//Player MyPlayer2;
+	//MyPlayer = &MyPlayer2;
+	//Player* MyPlayer3 = new Player;
+
+	//MyActor[0] = MyPlayer3;//변수명이 필요없다
+
+	//int a[10];
+	//a[0] = 1;
+
+	//int b = 2;
+	//a[0] = b;
+	//a[0] = 2;//이거랑 위랑 같은거임 2그냥 넣을거면 b는 이름 없어도 된다
+
+	MyActor.push_back(new Player);
+	MyActor.push_back(new Monster);
+	MyActor.push_back(new Wall);
+	MyActor.push_back(new Goal);
+
+	MyActor[0]->GetX == MyActor[i]
+		Shape
+
+
+		/*if (Map[Y][X] == MyPlayer[Y][X])
+		{
+			cout << P;
+		}
+
+
+		for(int i=0; i<10;++i)
+		{
+			for (int j = 0; j < 10; ++j)
+			{
+				if (Map[i][j] == 0)
+				{
+					cout << " ";
+				}
+				else if (Map[i][j] == 1)
+				{
+					cout << "*";
+				}
+
+				else if (Map[i][j] == 2)
+				{
+					cout << "P";
+				}
+			}cout << endl;
+		}
+		system("cls");*/
 }
